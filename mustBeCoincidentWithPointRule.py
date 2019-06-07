08bd9f5c7abcf82c96091abb37792fd3db906fbf
Mauro Carlevaro <mauroctecno@gmail.com>
2019-06-07 00:14:56 -0300

    1.0
    correct file mustBeCoincidentWithPointRule.py and add the corresponding Action.py

---
+++ mustBeCoincidentWithPointRule.py
@@ -0,0 +1,123 @@
+from gvsig import uselib
+uselib.use_plugin("org.gvsig.topology.app.mainplugin")
+
+import sys
+from org.gvsig.topology.lib.spi import AbstractTopologyRule
+from gvsig import logger
+from gvsig import LOGGER_WARN,LOGGER_INFO,LOGGER_ERROR
+from org.gvsig.topology.lib.api import TopologyLocator
+from org.gvsig.expressionevaluator import ExpressionEvaluatorLocator
+
+# from DeletePointAction import DeletePointAction
+
+class MustBeCoincidentWithPointRule(AbstractTopologyRule):
+  geomName = None
+  expression = None
+  expressionBuilder = None
+  
+  def __init__(self, plan, factory, tolerance, dataSet1, dataSet2):
+    #        TopologyPlan plan,
+    #        TopologyRuleFactory factory,
+    #        double tolerance,
+    #        String dataSet1
+    #        String dataSet2
+    
+    AbstractTopologyRule.__init__(self, plan, factory, tolerance, dataSet1, dataSet2)
+    # self.addAction(DeletePointAction())
+  
+  def check(self, taskStatus, report, feature1):
+    #SimpleTaskStatus taskStatus, 
+    #TopologyReport report, 
+    #Feature feature1
+
+    try:
+      logger("tak", LOGGER_INFO)
+      store2 = self.getDataSet2().getFeatureStore()
+      tolerance = self.getTolerance()
+      #logger("1", LOGGER_INFO)
+      
+      if (self.expression == None):
+        manager = ExpressionEvaluatorLocator.getManager()
+        self.expression = manager.createExpression()
+        self.expressionBuilder = manager.createExpressionBuilder()
+        self.geomName = store2.getDefaultFeatureType().getDefaultGeometryAttributeName()
+      
+      point = feature1.getDefaultGeometry()
+      pointTolerance = point.buffer(tolerance) #polygon
+      
+      if( point==None ):
+        return
+      #logger("1", LOGGER_INFO)
+      
+      theDataSet1 = self.getDataSet1()
+      theDataSet2 = self.getDataSet2()
+      #logger("2", LOGGER_INFO)
+      if theDataSet2.getSpatialIndex() != None:
+        #logger("if", LOGGER_INFO)
+        for reference in theDataSet2.query(point):
+            #FeatureReference reference
+            # Misma feature
+            #logger("ref"+str(reference), LOGGER_INFO)
+            if (reference.equals(feature1.getReference())):
+              continue;
+            
+            feature2 = reference.getFeature()
+            otherPoint = feature2.getDefaultGeometry()
+            if (otherPoint!=None and not pointTolerance.intersection(otherPoint)):
+              error = pointTolerance.disjoint(otherPoint)
+              report.addLine(self,
+                theDataSet1,
+                theDataSet2,
+                point,
+                error,
+                feature1.getReference(),
+                feature2.getReference(),
+                False,
+                "The point is not coincident."
+              )
+              break
+            
+      else:
+        logger("else", LOGGER_INFO)
+        self.expression.setPhrase(
+          self.expressionBuilder.ifnull(
+            self.expressionBuilder.column(self.geomName),
+            self.expressionBuilder.constant(False),
+            self.expressionBuilder.ST_Overlaps(
+              self.expressionBuilder.column(self.geomName),
+              self.expressionBuilder.geometry(point)
+            )
+          ).toString()
+        )
+        feature = theDataSet2.findFirst(self.expression)
+        if feature == None:
+            otherPoint = feature.getDefaultGeometry()
+            error = None
+            if otherPoint==None :
+              error = point.difference(otherPoint)
+            
+            report.addLine(self,
+              theDataSet1,
+              theDataSet2,
+              point,
+              error,
+              feature1.getReference(),
+              feature2.getReference(),
+              False,
+              "The point is not coincident."
+            )
+        logger("end", LOGGER_INFO)
+    except: # Exception as ex:
+      #logger("2 Can't check feature."+str(ex), LOGGER_WARN)
+      ex = sys.exc_info()[1]
+      logger("Can't execute rule. Class Name:" + ex.__class__.__name__ + " Except:" + str(ex))
+    finally:
+      pass
+def main(*args):
+  # testing class m = MustBeCoincidentWithPointRule(None, None, 3, None)
+  print "* Executing MustBeCoincidentWithPoint RULE main."
+  tm = TopologyLocator.getTopologyManager()
+  
+  from mustBeCoincidentWithPointRuleFactory import MustBeCoincidentWithPointRuleFactory
+  a = MustBeCoincidentWithPointRuleFactory()
+  tm.addRuleFactories(a)
---
+++ mustBeCoincidentWithPointRuleFactory.py
@@ -0,0 +1,49 @@
+# encoding: utf-8
+
+import gvsig
+from gvsig import uselib
+uselib.use_plugin("org.gvsig.topology.app.mainplugin")
+
+from org.gvsig.fmap.geom import Geometry
+from org.gvsig.tools.util import ListBuilder
+from org.gvsig.topology.lib.api import TopologyLocator
+from org.gvsig.topology.lib.api import TopologyManager
+from org.gvsig.topology.lib.spi import AbstractTopologyRuleFactory
+from org.gvsig.topology.lib.api import TopologyPlan
+from org.gvsig.topology.lib.api import TopologyRule
+
+from gvsig import logger
+from gvsig import LOGGER_WARN,LOGGER_INFO,LOGGER_ERROR
+
+from mustBeCoincidentWithPointRule import MustBeCoincidentWithPointRule
+from org.gvsig.topology.lib.api import TopologyLocator
+
+
+class MustBeCoincidentWithPointRuleFactory(AbstractTopologyRuleFactory):
+  #NAME = "MustBeCoincidentWithPoint"
+    
+  def __init__(self):
+    AbstractTopologyRuleFactory.__init__(
+      self,
+      "MustBeCoincidentWithPoint",
+      "Must Be Coincident With", 
+      "This rule requires that the points in both datasets must be coincident, if not, a points errors layer is created. This rule is used when points must coincide. It is useful for modelling networks like electricity. For example in an electricity or water supply network, the water and light meters must match with the service points.", 
+      ListBuilder().add(Geometry.TYPES.POINT).add(Geometry.TYPES.MULTIPOINT).asList(),
+	  ListBuilder().add(Geometry.TYPES.POINT).add(Geometry.TYPES.MULTIPOINT).asList()
+      )
+  def createRule(self, plan, dataSet1, dataSet2, tolerance):
+    #TopologyPlan plan, String dataSet1, String dataSet2, double tolerance
+    rule = MustBeCoincidentWithPointRule(plan, self, tolerance, dataSet1)
+    return rule
+
+def selfRegister():
+    try:
+      manager = TopologyLocator.getTopologyManager()
+      manager.addRuleFactories(MustBeCoincidentWithPointRuleFactory())
+    except Exception as ex:
+      logger("Can't register topology rule from MustBeCoincidentWithPointRuleFactory."+str(ex), LOGGER_WARN)
+
+def main(*args):
+  print "* Executing MustBeCoincidentWithPointRuleFactory main."
+  selfRegister()
+  pass
